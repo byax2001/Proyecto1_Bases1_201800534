4bbae7e587a3846c86b1d59dd393a8c886d4f2c7
Moonbbyte <63923585+Moonbbyte@users.noreply.github.com>
2023-03-13 01:07:39 -0600

    csf

---
+++ runable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#userid ORACLE username/password@IP:PUERTO/SID
+#control -- control file name
+#log -- log file name
+#bad -- bad file name
+sqlldr userid=ldecast/ldecast@34.30.194.63:1521/ORCL18  control = filecontrol.ctl
+#echo " "
+#echo -e "\e[96m  ENTER PARA CONTINUAR ... \e[0m"
+#read
--- src/consultas/consulta.js
+++ src/consultas/consulta.js
@@ -1,8 +1,8 @@
 Router = require('express')
 const oracledb = require('oracledb');
-const fs = require('fs')
-const path = require('path')
 const router = Router()
+const { exec } = require('child_process');
+const path = require('path');
 
 const dbConfig = {
     user: 'ldecast',
@@ -13,13 +13,7 @@ const dbConfig = {
 
 router.get("/consulta1",async function(req,res){
     //http://localhost:8080/consultas/consulta1
-    let connection;
-
-    try {
-        // establecer la conexión con la base de datos
-        connection = await oracledb.getConnection(dbConfig);
-        // realizar la consulta
-        const consulta = `
+    const consulta = `
         SELECT h.nombre AS Hospital, u.UBICACION, COUNT(v.id_victima) AS Fallecidos
         FROM Hospital h
         LEFT JOIN Ubicacion u ON h.id_ubicacion = u.id_ubicacion
@@ -27,100 +21,27 @@ router.get("/consulta1",async function(req,res){
         JOIN Victima v ON hv.id_victima = v.id_victima
         WHERE v.fecha_muerte IS NOT NULL
         GROUP BY u.UBICACION, h.nombre
-        ORDER BY Fallecidos DESC
+        ORDER BY Fallecidos desc
         `;
-        const result = await connection.execute(consulta);
-        const rows = result.rows;
-        const columnNames = result.metaData.map(column => column.name);
-        const data = rows.map(row => {
-        return columnNames.reduce((obj, column, index) => {
-            obj[column] = row[index];
-            return obj;
-        }, {});
-        });
-        // cerrar la conexión
-        await connection.close();
-        // devolver el resultado como un JSON
-        return res.json(data);
-        return res.json(result.rows);
-    } catch (err) {
-        console.error(err.message);
-        if (connection) {
-        await connection.close();
-        }
-        return res.status(500).send('Error al realizar la consulta');
-    }
+    m_consulta(req,res,consulta)
 })
 router.get("/consulta2",async function(req,res){
-    let connection;
-
-    try {
-        // establecer la conexión con la base de datos
-        connection = await oracledb.getConnection(dbConfig);
-        // realizar la consulta
-        var consulta = `SELECT v.NOMBRE, v.APELLIDO, v.STATUS_ENFERMEDAD, vt.EFECTIVIDAD_EN_VICTIMA 
-        FROM VICTIMA v 
-        JOIN VICTIMA_TRATAMIENTO vt ON v.ID_VICTIMA = vt.ID_VICTIMA 
-        JOIN TRATAMIENTO t ON vt.ID_TRATAMIENTO = t.ID_TRATAMIENTO 
-        WHERE v.STATUS_ENFERMEDAD = 'En cuarentena' AND t.NOMBRE = 'Transfusiones de sangre' AND vt.EFECTIVIDAD_EN_VICTIMA > 5`
-        const result = await connection.execute(consulta);
-        const rows = result.rows;
-        const columnNames = result.metaData.map(column => column.name);
-        const data = rows.map(row => {
-        return columnNames.reduce((obj, column, index) => {
-            obj[column] = row[index];
-            return obj;
-        }, {});
-        });
-        // cerrar la conexión
-        await connection.close();
-        // devolver el resultado como un JSON
-        return res.json(data);
-        return res.json(result.rows);
-    } catch (err) {
-        console.error(err.message);
-        if (connection) {
-        await connection.close();
-        }
-        return res.status(500).send('Error al realizar la consulta');
-    }
+    var consulta = `SELECT v.NOMBRE, v.APELLIDO, vt.EFECTIVIDAD_EN_VICTIMA 
+    FROM VICTIMA v 
+    JOIN VICTIMA_TRATAMIENTO vt ON v.ID_VICTIMA = vt.ID_VICTIMA 
+    JOIN TRATAMIENTO t ON vt.ID_TRATAMIENTO = t.ID_TRATAMIENTO 
+    WHERE v.STATUS_ENFERMEDAD = 'En cuarentena' AND t.NOMBRE = 'Transfusiones de sangre' AND vt.EFECTIVIDAD_EN_VICTIMA > 5`
+    m_consulta(req,res,consulta)
 })
 router.get("/consulta3",async function(req,res){
-    let connection;
-
-    try {
-        // establecer la conexión con la base de datos
-        connection = await oracledb.getConnection(dbConfig);
-        // realizar la consulta
-        var consulta = `SELECT v.NOMBRE, V.APELLIDO , u.Ubicacion, COUNT(av.ID_ALLEGADO) AS ALLEGADOS 
-        FROM VICTIMA v 
-        JOIN UBICACION u ON u.ID_UBICACION =v.ID_DIRECCION 
-        JOIN ALLEGADO_VICTIMA av ON av.ID_VICTIMA = V.ID_VICTIMA 
-        WHERE v.FECHA_MUERTE IS not NULL 
-        GROUP BY v.NOMBRE , V.APELLIDO, U.UBICACION 
-        HAVING COUNT(DISTINCT av.ID_ALLEGADO) > 3  ORDER BY allegados`
-        const result = await connection.execute(consulta);
-        const rows = result.rows;
-        const columnNames = result.metaData.map(column => column.name);
-        const data = rows.map(row => {
-        return columnNames.reduce((obj, column, index) => {
-            obj[column] = row[index];
-            return obj;
-        }, {});
-        });
-        // cerrar la conexión
-        await connection.close();
-        // devolver el resultado como un JSON
-        return res.json(data);
-        return res.json(result.rows);
-    } catch (err) {
-        console.error(err.message);
-        if (connection) {
-        await connection.close();
-        }
-        return res.status(500).send('Error al realizar la consulta');
-    }
-
+    var consulta = `SELECT v.NOMBRE, V.APELLIDO , u.Ubicacion, COUNT(av.ID_ALLEGADO) AS ALLEGADOS 
+    FROM VICTIMA v 
+    JOIN UBICACION u ON u.ID_UBICACION =v.ID_DIRECCION 
+    JOIN ALLEGADO_VICTIMA av ON av.ID_VICTIMA = V.ID_VICTIMA 
+    WHERE v.FECHA_MUERTE IS not NULL 
+    GROUP BY v.NOMBRE , V.APELLIDO, U.UBICACION 
+    HAVING COUNT(DISTINCT av.ID_ALLEGADO) > 3  ORDER BY allegados`
+    m_consulta(req,res,consulta)
 })
 router.get("/consulta4",async function(req,res){
     let consulta =`SELECT v.NOMBRE, V.APELLIDO ,v.STATUS_ENFERMEDAD 
@@ -227,16 +148,330 @@ router.get("/consulta10",async function(req,res){
     m_consulta(req,res,consulta)
 })
 router.get("/eliminarTemporal",async function(req,res){
+    let connection;
+    try {
+        // establecer la conexión con la base de datos
+        connection = await oracledb.getConnection(dbConfig);
+        // realizar la consulta
+        await connection.execute("DROP TABLE TEMPORAL");
+        await connection.execute(`CREATE TABLE TEMPORAL (
+            NOMBRE_VICTIMA VARCHAR(60),
+            APELLIDO_VICTIMA VARCHAR(60),
+            DIRECCION_VICTIMA VARCHAR(100),
+            FECHA_PRIMERA_SOSPECHA DATE,
+            FECHA_CONFIRMACION DATE,
+            FECHA_MUERTE DATE,
+            ESTADO_VICTIMA VARCHAR(60),
+            NOMBRE_ASOCIADO VARCHAR(60),
+            APELLIDO_ASOCIADO VARCHAR(60),
+            FECHA_CONOCIO  DATE,
+            CONTACTO_FISICO VARCHAR(60),
+            FECHA_INICIO_CONTACTO DATE ,
+            FECHA_FIN_CONTACTO DATE ,
+            NOMBRE_HOSPITAL VARCHAR(60),
+            DIRECCION_HOSPITAL VARCHAR(100),
+            UBICACION_VICTIMA VARCHAR(100),
+            FECHA_LLEGADA DATE,
+            FECHA_RETIRO DATE,
+            TRATAMIENTO VARCHAR(60),
+            EFECTIVIDAD int,
+            FECHA_INICIO_TRATAMIENTO DATE,
+            FECHA_FIN_TRATAMIENTO DATE,
+            EFECTIVIDAD_EN_VICTIMA INT
+          )`);
+        await connection.close();
 
+        // devolver el resultado como un JSON
+        res.json({estado:"Temporal Eliminada"})
+    } catch (err) {
+        console.error(err.message);
+        if (connection) {
+        await connection.close();
+        }
+        return res.status(500).send('Error al realizar la consulta');
+    }
 })
 router.get("/eliminarModelo",async function(req,res){
+    let connection;
+    try {
+        // establecer la conexión con la base de datos
+        connection = await oracledb.getConnection(dbConfig);
+        // realizar la consulta
+        await connection.execute('DROP TABLE Ubicacion CASCADE CONSTRAINTS');
+        await connection.execute('DROP TABLE TRATAMIENTO CASCADE CONSTRAINTS');
+        await connection.execute('DROP TABLE tipo_contacto CASCADE CONSTRAINTS');
+        await connection.execute('DROP TABLE Allegado CASCADE CONSTRAINTS');
+        await connection.execute('DROP TABLE hospital CASCADE CONSTRAINTS');
+        await connection.execute('DROP TABLE Victima CASCADE CONSTRAINTS');
+        await connection.execute('DROP TABLE REGISTRO CASCADE CONSTRAINTS');
+        await connection.execute('DROP TABLE victima_tratamiento CASCADE CONSTRAINTS');
+        await connection.execute('DROP TABLE Lugar_victima CASCADE CONSTRAINTS');
+        await connection.execute('DROP TABLE Allegado_victima CASCADE CONSTRAINTS');
+        await connection.close();
 
+        // devolver el resultado como un JSON
+        res.json({estado:"Modelo Eliminado con Exito"})
+    } catch (err) {
+        console.error(err.message);
+        if (connection) {
+        await connection.close();
+        }
+        return res.status(500).send('Error al realizar la consulta');
+    }
 })
 router.get("/cargarTemporal",async function(req,res){
-
+    const connectionString = 'ldecast/ldecast@34.30.194.63:1521/ORCL18';
+    const controlFile = 'filecontrol.ctl';
+    const dataFile = 'DB_Excel.csv';
+    const command = `sqlldr ${connectionString} control=${controlFile} data=${dataFile}`;
+    
+    exec(command, (err, stdout, stderr) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log(stdout);
+      return res.json({Carga:"carga exitosa"})
+    });
 })
 router.get("/cargarModelo",async function(req,res){
+    let connection;
+    try {
+        // establecer la conexión con la base de datos
+        connection = await oracledb.getConnection(dbConfig);
+        // realizar la consulta
+        await connection.execute(`
+        CREATE TABLE Ubicacion(
+            id_ubicacion INTEGER GENERATED BY DEFAULT as identity (START WITH 1 INCREMENT BY 1) ,
+            ubicacion VARCHAR(60),
+            CONSTRAINT pk_ubicacion PRIMARY KEY(id_ubicacion)
+        )`);
+        
+        await connection.execute(`
+        CREATE TABLE Tratamiento (
+            id_tratamiento INTEGER GENERATED BY DEFAULT as identity (START WITH 1 INCREMENT BY 1) ,
+            nombre VARCHAR(50),
+            efectividad int,
+            PRIMARY KEY(id_tratamiento)
+        )`);
+        
+        await connection.execute(`
+        CREATE Table Tipo_contacto(
+            id_tipocontacto INTEGER GENERATED BY DEFAULT as identity (START WITH 1 INCREMENT BY 1) ,
+            tipo_contacto VARCHAR(50),
+            PRIMARY key(id_tipocontacto)
+        )`);
+        
+        await connection.execute(`
+        CREATE Table Allegado(
+            id_allegado INTEGER GENERATED BY DEFAULT as identity (START WITH 1 INCREMENT BY 1) ,
+            nombre VARCHAR(50),
+            apellido VARCHAR(50),
+            PRIMARY KEY(id_allegado)
+        )`);
+        
+        await connection.execute(`
+        CREATE TABLE Hospital (
+            id_hospital INTEGER GENERATED BY DEFAULT as identity (START WITH 1 INCREMENT BY 1) ,
+            nombre VARCHAR(60),
+            id_ubicacion int, 
+            PRIMARY KEY (id_hospital),
+            FOREIGN KEY (id_ubicacion) REFERENCES Ubicacion(id_ubicacion) 
+        )`);
+        
+        await connection.execute(`
+        CREATE TABLE Victima (
+            id_victima INTEGER GENERATED BY DEFAULT as identity (START WITH 1 INCREMENT BY 1) ,
+            nombre VARCHAR(60),
+            apellido VARCHAR(60),
+            id_direccion int,
+            fecha_primera_sospecha date,
+            fecha_confirmacion date,
+            fecha_muerte date,
+            status_enfermedad VARCHAR(50),
+            PRIMARY key(id_victima),
+            FOREIGN KEY (id_direccion) REFERENCES Ubicacion(id_ubicacion)
+        )`);
+        
+        await connection.execute(`
+        CREATE TABLE Registro(
+            id_registro INTEGER GENERATED BY DEFAULT as identity (START WITH 1 INCREMENT BY 1) ,
+            id_hospital int,
+            id_victima int,
+            PRIMARY KEY(id_registro),
+            FOREIGN KEY (id_hospital) REFERENCES Hospital(id_hospital) ,
+            FOREIGN KEY (id_victima) REFERENCES victima(id_victima) 
+        )`);
+        
+        await connection.execute(`
+        CREATE TABLE victima_tratamiento(
+            id_p_tratamiento INTEGER GENERATED BY DEFAULT as identity (START WITH 1 INCREMENT BY 1) ,
+            id_victima int,
+            id_tratamiento int,
+            efectividad_en_victima int,
+            FECHA_INICIO_TRATAMIENTO DATE,
+            FECHA_FIN_TRATAMIENTO DATE,
+            PRIMARY KEY(id_p_tratamiento),
+            FOREIGN KEY (id_victima) REFERENCES victima(id_victima),
+            FOREIGN KEY (id_tratamiento) REFERENCES Tratamiento(id_tratamiento)
+        )`);
+        
+        await connection.execute(`
+        CREATE TABLE Lugar_victima(
+            id_lugarvictima INTEGER GENERATED BY DEFAULT as identity (START WITH 1 INCREMENT BY 1) ,
+            id_ubicacion int,
+            id_victima int,
+            fecha_llegada date,
+            fecha_retiro date,
+            PRIMARY KEY(id_lugarvictima),
+            FOREIGN KEY (id_ubicacion) REFERENCES Ubicacion(id_ubicacion),
+            FOREIGN KEY (id_victima) REFERENCES VICTIMA(id_victima)
+        )`);
+        
+        await connection.execute(`
+        CREATE TABLE Allegado_victima(
+            id_allegadovictima INTEGER GENERATED BY DEFAULT as identity (START WITH 1 INCREMENT BY 1) ,
+            id_victima int,
+            id_allegado int,
+            id_tipocontacto int,
+            fecha_conocio date,
+            fecha_inicio_contacto date,
+            fecha_fin_contacto date,
+            PRIMARY KEY(id_allegadovictima),
+            FOREIGN KEY (id_victima) REFERENCES victima(id_victima),
+            FOREIGN KEY (id_allegado) REFERENCES Allegado(id_allegado),
+            FOREIGN KEY (id_tipocontacto) REFERENCES Tipo_contacto(id_tipocontacto)
+        )`);
+        
+        //RELLENAR TABLAS
+        await connection.execute(`INSERT INTO Ubicacion(ubicacion)
+        SELECT DISTINCT temporal.direccion_victima FROM TEMPORAL union
+        select DISTINCT temporal.direccion_hospital FROM temporal union
+        select DISTINCT temporal.ubicacion_victima FROM TEMPORAL
+        WHERE 
+            temporal.direccion_victima is not null AND 
+            temporal.direccion_hospital is not null AND 
+            temporal.ubicacion_victima is not null 
+        `);
+        
 
+        await connection.execute(`INSERT INTO Tratamiento (nombre,efectividad) 
+        SELECT DISTINCT
+            temporal.TRATAMIENTO,
+            temporal.EFECTIVIDAD
+        FROM temporal
+        WHERE 
+            temporal.tratamiento is not null AND 
+            temporal.EFECTIVIDAD is not null
+        `);
+
+
+        await connection.execute(`INSERT INTO Tipo_contacto(tipo_contacto) 
+        SELECT DISTINCT
+            temporal.CONTACTO_FISICO
+        FROM temporal
+        WHERE 
+            temporal.CONTACTO_FISICO is not null
+        `);
+
+
+        await connection.execute(`INSERT INTO ALLEGADO(nombre,apellido) 
+        SELECT DISTINCT 
+            TEMPORAL.NOMBRE_ASOCIADO,
+            TEMPORAL.APELLIDO_ASOCIADO
+        FROM TEMPORAL
+        WHERE 
+            TEMPORAL.NOMBRE_ASOCIADO is not null
+        `);
+
+        await connection.execute(`INSERT INTO Hospital (id_ubicacion, nombre) 
+        SELECT DISTINCT
+            ubicacion.id_ubicacion,   
+            temporal.NOMBRE_HOSPITAL 
+        FROM Temporal, Ubicacion
+        WHERE 
+            temporal.NOMBRE_HOSPITAL is not null AND 
+            temporal.DIRECCION_HOSPITAL = Ubicacion.ubicacion 
+        `);
+
+        await connection.execute(`INSERT INTO Victima (nombre, apellido, id_direccion, fecha_primera_sospecha, fecha_confirmacion, fecha_muerte,status_enfermedad) 
+        SELECT DISTINCT 
+            temporal.NOMBRE_VICTIMA,
+            temporal.APELLIDO_VICTIMA,
+            ubicacion.id_ubicacion,
+            temporal.FECHA_PRIMERA_SOSPECHA,
+            temporal.FECHA_CONFIRMACION,
+            TEMPORAL.FECHA_MUERTE,
+            TEMPORAL.ESTADO_VICTIMA
+        FROM temporal, Ubicacion
+        WHERE
+        TEMPORAL.DIRECCION_VICTIMA = Ubicacion.ubicacion
+        `);
+        await connection.execute(`INSERT INTO Registro(id_hospital,id_victima)
+        SELECT DISTINCT 
+            Hospital.id_hospital,
+            victima.id_victima
+        FROM Temporal, Hospital, victima 
+        WHERE 
+            temporal.NOMBRE_HOSPITAL = Hospital.nombre and
+            temporal.NOMBRE_VICTIMA = Victima.nombre AND 
+            TEMPORAL.APELLIDO_VICTIMA = Victima.APELLIDO 
+        `);
+
+
+        await connection.execute(`INSERT INTO victima_tratamiento(id_victima,id_tratamiento,efectividad_en_victima,FECHA_INICIO_TRATAMIENTO,FECHA_FIN_TRATAMIENTO)
+        SELECT DISTINCT 
+            Victima.id_victima,
+            Tratamiento.id_tratamiento,
+            TEMPORAL.EFECTIVIDAD_EN_VICTIMA,
+            TEMPORAL.FECHA_INICIO_TRATAMIENTO,
+            TEMPORAL.FECHA_FIN_TRATAMIENTO
+        FROM Temporal, TRATAMIENTO, victima 
+        WHERE 
+            TEMPORAL.NOMBRE_VICTIMA = VICTIMA.NOMBRE AND
+            TEMPORAL.APELLIDO_VICTIMA = VICTIMA.APELLIDO AND 
+            TEMPORAL.TRATAMIENTO = TRATAMIENTO.NOMBRE 
+        `);
+
+        await connection.execute(`INSERT INTO Lugar_victima(id_victima,id_ubicacion,fecha_llegada,fecha_retiro)
+        SELECT DISTINCT 
+            Victima.id_victima,
+            Ubicacion.ID_UBICACION,
+            TEMPORAL.FECHA_LLEGADA,
+            TEMPORAL.FECHA_RETIRO
+        FROM Temporal, Ubicacion, victima 
+        WHERE 
+            TEMPORAL.UBICACION_VICTIMA = Ubicacion.ubicacion and
+            TEMPORAL.NOMBRE_VICTIMA  = Victima.nombre AND 
+            TEMPORAL.APELLIDO_VICTIMA  = Victima.APELLIDO 
+        `);
+
+
+        await connection.execute(`INSERT INTO ALLEGADO_victima(id_victima,id_allegado,id_tipocontacto,fecha_inicio_contacto,fecha_fin_contacto, fecha_conocio)
+        SELECT DISTINCT 
+            Victima.id_victima,
+            Allegado.id_allegado,
+            Tipo_contacto.id_tipocontacto,
+            TEMPORAL.FECHA_INICIO_CONTACTO,
+            TEMPORAL.FECHA_FIN_CONTACTO,
+            TEMPORAL.FECHA_CONOCIO
+        FROM temporal,Victima,Allegado,TIPO_CONTACTO
+        WHERE 
+            TEMPORAL.NOMBRE_VICTIMA = VICTIMA.NOMBRE  AND
+            temporal.APELLIDO_VICTIMA = victima.APELLIDO AND
+            TEMPORAL.NOMBRE_ASOCIADO = ALLEGADO.NOMBRE  AND
+            temporal.APELLIDO_ASOCIADO = allegado.APELLIDO AND 
+            TEMPORAL.CONTACTO_FISICO = TIPO_CONTACTO.TIPO_CONTACTO`);
+        connection.close();
+        // devolver el resultado como un JSON
+        res.json({estado:"Tablas creadas y cargadas"})
+    } catch (err) {
+        console.error(err.message);
+        if (connection) {
+        await connection.close();
+        }
+        return res.status(500).send('Error al realizar la consulta');
+    }
 })
 
 m_consulta=async(req,res,consulta)=>{
